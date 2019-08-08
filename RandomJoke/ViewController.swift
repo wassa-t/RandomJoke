ae07f6efd2a7e2f1a82a93a61dc6ddd38131b512
Wassa Tunsiri <>
2019-08-08 14:44:40 +0700

    Initial commit

---
+++ RandomJoke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomJoke
+//
+//  Created by Wassa Tunsiri on 8/8/2562 BE.
+//  Copyright © 2562 Wassa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
